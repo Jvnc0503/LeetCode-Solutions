f02423bea7b86774f38adf83ddc65f0af8ce0d2c
Jvnc0503 <jvnc0503@gmail.com>
2025-04-19 14:02:23 -0500

    commit

--- 1977. Number of Ways to Separate Numbers.h
+++ 1977. Number of Ways to Separate Numbers.h
@@ -18,38 +18,51 @@ public:
         return true;
     }
 
+    bool compareFaster(string& num, int startPrev, int startLast, int len) {
+        return memcmp(&num[startPrev], &num[startLast], len) <= 0;
+    }
+
     int numberOfCombinations(string num) {
         int n = num.size();
-        vector<vector<int>> dp(n, vector<int>(n+1, 0));
         // dp[i][j] number of ways to split string with the last number ends at i and has length l
+        //vector<vector<int>> dp(n, vector<int>(n+1, 0));
+        // preSum[i][j] number of ways to split string with last number ending at i with length from 1 t
+        vector<vector<int>> preSum(n, vector<int>(n+1, 0));
         for (int i = 0; i < n; ++i) {
             for (int l = 1; l <= i + 1; ++ l) {
                 int j = i - l + 1;  // Position where last number starts
+                int cur = 0;    // Save ways for current dp[i][j]
 
                 // If last numbers starts with 0 it is not valid
                 if (num[j] == '0') {
-                    dp[i][l] = 0;
+                    //dp[i][l] = 0;
+                    cur = 0;
                 } else if (j == 0) {
-                    dp[i][l] = 1;   // Base case, last numbers start at position 0
+                    //dp[i][l] = 1;   // Base case, last numbers start at position 0
+                    cur = 1;
                 } else {
                     // Max length the second last number can have so it can be less or equal to last number
                     int maxl2 = 0;
                     // If last number starts at a position with value lower than l, the previous number is always smaller
                     if (j < l) {
                         maxl2 = j;
-                    } else if (compare(num, j-l, j, l)) {
+                    } else if (compareFaster(num, j-l, j, l)) {
                         maxl2 = l; // If prev number is less or equal than last, we consider max length as l
                     } else {
                         maxl2 = l - 1;  // If prev is more than last, we onsidera max length as l - 1
                     }
-                    for (int l2 = 0; l2 <= maxl2; ++l2) {
+                    /*for (int l2 = 0; l2 <= maxl2; ++l2) {
                         // We add the ways to split the string with the last number ending at j-1 with length l2
                         dp[i][l] = (dp[i][l] + dp[j-1][l2]) % mod;
-                    }
+                    }*/
+                    //dp[i][l] = preSum[j-1][maxl2];
+                    cur = preSum[j-1][maxl2];
                 }
+                //preSum[i][l] = (preSum[i][l-1] + dp[i][l]) % mod;
+                preSum[i][l] = (preSum[i][l-1] + cur) % mod;
             }
         }
         // Return the sum of ways to split string with last number ending at n-1 with size[1,n]
-        return accumulate(dp[n-1].begin(), dp[n-1].end(), 0);
+        return preSum[n-1][n] % mod;
     }
 };
