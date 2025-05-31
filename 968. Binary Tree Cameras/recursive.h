56a8733a1394f2e91302f5d9f263b467e3010001
Jvnc0503 <jvnc0503@gmail.com>
2025-05-31 17:10:22 -0500

    Initial commit for problem 968

---
+++ 968. Binary Tree Cameras/recursive.h
@@ -0,0 +1,45 @@
+/**
+* Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    enum Coverage {NOT_COVERED, CAMERA, COVERED};
+    int cameras = 0;
+
+    Coverage dfs(TreeNode* root) {
+        if (root == nullptr) {
+            // If current node doesn't exist, we consider it as covered
+            return COVERED;
+        }
+        // Get the coverage of children
+        int left = dfs(root->left);
+        int right = dfs(root->right);
+
+        if (left == NOT_COVERED || right == NOT_COVERED) {
+            // If any of the children is not covered, we create a new camera on the current node
+            ++cameras;
+            return CAMERA;
+        }
+        if (left == CAMERA || right == CAMERA){
+            // If any of the children has a camera, then the current node is already covered
+            return COVERED;
+        }
+        // Node is no covered by any camera
+        return NOT_COVERED;
+    }
+
+    int minCameraCover(TreeNode* root) {
+        if (dfs(root) == NOT_COVERED) {
+            ++cameras;
+        }
+        return cameras;
+    }
+};
